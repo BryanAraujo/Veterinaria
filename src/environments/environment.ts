458267e12153e6b7de965e262f8e50fac302d992
Luis Chévez <luigichevez@gmail.com>
2020-10-21 02:49:25 -0600

    Preparando para merge

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,6 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-<<<<<<< HEAD
   production: true,
   firebase: {
     apiKey: "AIzaSyAasPST9YwnxMfyzf2IRigSoLvpvav9FUY",
@@ -15,19 +14,7 @@ export const environment = {
       appId: "1:261388889827:web:1ecd043672319415559fd6",
       measurementId: "G-VDKL2VDXHT"
   }
-=======
-  production: false,
-  firebaseConfig : {
-    apiKey: "AIzaSyAasPST9YwnxMfyzf2IRigSoLvpvav9FUY",
-    authDomain: "veterinaria-4be17.firebaseapp.com",
-    databaseURL: "https://veterinaria-4be17.firebaseio.com",
-    projectId: "veterinaria-4be17",
-    storageBucket: "veterinaria-4be17.appspot.com",
-    messagingSenderId: "261388889827",
-    appId: "1:261388889827:web:1ecd043672319415559fd6",
-    measurementId: "G-VDKL2VDXHT"
-    }
->>>>>>> 9a3cdc04eea54cdef046fdc171de03547dd14ca8
+
 };
 
 /*
