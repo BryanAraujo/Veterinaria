17e35c5ee387f009cb3b7c5e61eca3589e48865a
Luis Chévez <luigichevez@gmail.com>
2020-11-22 19:36:32 -0600

    Routa funcionando

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,11 @@ import {ForgotPasswordComponent} from '../app/components/forgot-password/forgot-
 import {VerifyEmailComponent} from '../app/components/verify-email/verify-email.component';
 import {AuthService} from '../app/services/auth.service';
 import {ClientesComponent} from '../app/components/cliente/clientes/clientes.component';
+import {ProductoComponent}from'../app/components/productos/producto/producto.component';
+import {PropietarioComponent}from'../app/components/cliente/propietario/propietario.component';
+import {CitaComponent} from '../app/components/citas/cita/cita.component';
+import {RazaComponent}from '../app/components/cliente/raza/raza.component';
+import{ProveedoresComponent}from '../app/components/productos/proveedores/proveedores.component';
 import {VeterinarioComponent} from '../app/components/veterinario/veterinario.component';
 import {AuthGuard} from '../app/shared/guards/auth.guard';
 
@@ -16,6 +21,11 @@ const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
   { path: 'Clientes', component: ClientesComponent },
+  { path: 'Productos', component: ProductoComponent },
+  { path: 'Proveedores', component: ProveedoresComponent },
+  { path: 'Propietario', component: PropietarioComponent},
+  { path: 'Citas', component:CitaComponent},
+  { path: 'Raza', component:RazaComponent},
   { path: 'forgot-password', component: ForgotPasswordComponent },
   { path: 'verify-email-address', component: VerifyEmailComponent },
   { path: 'veterinario', component: VeterinarioComponent}
